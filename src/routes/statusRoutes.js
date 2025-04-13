694082ae6b32d79c99bc418d28d3d6f56f7fbbef
frogfather <j.a.campbell@icloud.com>
2025-04-13 09:16:59 +0100

    basic routing works

--- src/routes/statusRoutes.js
+++ src/routes/statusRoutes.js
@@ -1,8 +1,13 @@
 import express from "express";
+import * as OnOff from 'onoff';
 
+const Gpio = OnOff.Gpio;
 const router = express.Router();
+const output2 = new Gpio(573,'out');
+let interval
 
 router.get("/", (req,res) => {
+  console.log('get base route');
     res.send(`
     <!DOCTYPE html>
       <html lang="en">
@@ -32,12 +37,41 @@ router.get("/", (req,res) => {
         <body>
           <div>
             <h1>🎉</h1>
-            <h1>Admin Backend is Running</h1>
+            <h1>Server is Running</h1>
             <p>API is available and serving requests.</p>
           </div>
         </body>
     </html>
     `)
 });
+router.get("/pins/2/start",(req,res) => {
+    interval = setInterval(toggle,3000);
+    res.status(200).json({
+        status: "started"
+      })
+  });
+
+router.get("/pins/2/stop",(req,res) => {
+    endToggle();
+    res.status(200).json({
+        status: "stopped"
+      })
+  })
+  
+  const toggle = () => {
+    if (output2.readSync() === 0) {
+      console.log('switch on');
+        output2.writeSync(1);
+      } else {
+        console.log('switch off');
+        output2.writeSync(0);
+      }
+    };
+  
+  const endToggle = () => {
+      clearInterval(interval);
+      output2.writeSync(0);
+      output2.unexport();
+  }
 
 export { router as statusRoutes };
